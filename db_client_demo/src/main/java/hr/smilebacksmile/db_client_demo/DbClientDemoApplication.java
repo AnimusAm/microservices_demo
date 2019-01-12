8698524f16303cfb779fef5c34c7973c70e0fedc
Animus <edu@test.com>
2019-01-12 19:10:58 +0100

    Testing how changes in modules reflect in git

--- db_client_demo/src/main/java/hr/smilebacksmile/db_client_demo/DbClientDemoApplication.java
+++ db_client_demo/src/main/java/hr/smilebacksmile/db_client_demo/DbClientDemoApplication.java
@@ -1,16 +1,12 @@
 package hr.smilebacksmile.db_client_demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DbClientDemoApplication {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(DbClientDemoApplication.class);
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         SpringApplication.run(DbClientDemoApplication.class, args);
     }
 
