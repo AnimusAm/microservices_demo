0aa6a746fa2e3a8297ff8953888a0a3dd3f362e8
Animus <edu@test.com>
2019-01-27 19:20:20 +0100

    Renamed folder of application_server to application_servers. Created discovery server app: discovery_server. Configured both client_service_vaadin_app and nosql_db_service as eureca discovery clients. Both pull discovery server configuration from configuration server. And changed client_service_vaadin_app to fetch data from nosql_db_service using eureca provided URI.

--- application_clients/client_service_vaadin_app/src/main/java/hr/smilebacksmile/ClientServiceVaadinAppApplication.java
+++ application_clients/client_service_vaadin_app/src/main/java/hr/smilebacksmile/ClientServiceVaadinAppApplication.java
@@ -1,24 +1,25 @@
 package hr.smilebacksmile;
 
+import com.netflix.discovery.DiscoveryClient;
 import hr.smilebacksmile.domain.TestData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.util.List;
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClientServiceVaadinAppApplication {
-    private static final Logger log = LoggerFactory.getLogger(ClientServiceVaadinAppApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ClientServiceVaadinAppApplication.class, args);
-
-        RestTemplate restTemplate = new RestTemplate();
-        TestData[] data = restTemplate.getForObject("http://localhost:8005/data", TestData[].class);
-        for (TestData d : data) {
-            log.info(d.toString());
-        }
     }
 
 }
--- application_servers/nosql_db_service/src/main/java/hr/smilebacksmile/NosqlDbServiceApplication.java
+++ application_servers/nosql_db_service/src/main/java/hr/smilebacksmile/NosqlDbServiceApplication.java
@@ -1,14 +1,12 @@
 package hr.smilebacksmile;
 
-import hr.smilebacksmile.dao.TestDataRepository;
-import hr.smilebacksmile.domain.TestData;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NosqlDbServiceApplication {
-
 
     public static void main(String[] args)  {
         SpringApplication.run(NosqlDbServiceApplication.class, args);
